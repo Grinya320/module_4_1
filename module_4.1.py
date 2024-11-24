8782f54b2cf4af07707802b33e83ec4e2d33a31d
Grinya320 <grilya320@gmail.com>
2024-11-24 14:17:58 +0300

    Add files via upload

---
+++ module_4.1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
+
+result1 = fake_divide(56, 8)
+result2 = fake_divide(95,0)
+result3 = true_divide(25, 5)
+result4 = true_divide(97, 0)
+print(result1)
+print(result2)
+print(result3)
+print()
